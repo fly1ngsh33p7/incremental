a71ad3b5986a2596a3d7c2f058469b60f60248d7
Thomas Klaus <fly1ngsh33p7@tutanota.com>
2026-09-05 20:13:53 +0200

    cleanup of example Components

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import Game from './Game.vue';
-import router from './router';
 import { useResourcesStore } from './stores/resources.ts';
 import { Resource, type CostFunction } from './Resource.ts';
 
 const game = createApp(Game);
 
 game.use(createPinia());
-game.use(router);
 
 game.mount('#game');
